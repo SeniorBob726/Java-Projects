803239d206d99a6c3b32604ca7e9376bd54503c4
Taha Vasowalla <tvdood99@gmail.com>
2015-10-21 21:09:12 -0400

    Finish Chapter 7

--- Chapter7.java
+++ Chapter7.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Chapter7 {
@@ -28,11 +30,106 @@ public class Chapter7 {
 		System.out.println(addend + " = " + sum);
 	}
 
-	
+	public static boolean isPrime(int n) {
+		if(n <= 1) {
+			return false;
+		}
+		else if(n == 2) {
+			return true;
+		}
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		sumTo(scanner.nextInt());
+		int m = 3;
+		while(m * m <= n) {
+			if(n % m == 0) {
+				return false;
+			}
+
+			m += 2;
+		}
+
+		return true;
+	}
+
+	public static boolean isPrimeEfficient(int n) {
+		return false;
+	}
+
+	public static int sumDigits(int n) {
+		int sum = 0;
+		while(n > 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum;
+	}
+
+	public static double averageScore() {
+		Scanner input = null;
+
+		try {
+			input = new Scanner(new File("scores.dat"));
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Can't open scores.dat");
+			System.exit(1);
+		}
+
+		double average = 0;
+		int i = 0;
+
+		while(input.hasNextInt()) {
+			average += input.nextInt();
+			i++;
+		}
+
+		return average / i;
+	}
+
+	public static String twinPrimes() {
+		String output = "";
+		int i = 0;
+
+		int n = 3;
+		int lastPrime = 3;
+
+		while(i < 20) {
+			if(isPrime(n)) {
+				if(n - lastPrime == 2) {
+					output += lastPrime + " and " + n + ", ";
+					i++;
+				}
+				lastPrime = n;
+				n += 2;
+			}
+			else {
+				n += 2;
+			}
+		}
+
+		return output.substring(0, output.length() - 2);
+	}
+
+	public static int flowchartOutput(int n, int b) {
+		int p = 1;
+
+		while(p <= n) {
+			n -= p;
+			p *= b;
+		}
+
+		return n;
+	}
+
+	public static double goldenRatio(int n) { // n = 10 has a difference of 5.646e-5 from the golden ratio
+		double xprev;
+		double x = 1;
+
+		while(n > 0) {
+			xprev = x;
+			x = 1 + 1.0 / xprev;
+			n--;
+		}
+
+		return x;
 	}
 }
