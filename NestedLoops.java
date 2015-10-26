bf605e720a844f6f325d8766ad09574e0035ef09
DarkAce65 <tvdood99@gmail.com>
2015-10-26 10:36:33 -0400

    Create factorial

--- NestedLoops.java
+++ NestedLoops.java
@@ -1,4 +1,13 @@
 public class NestedLoops {
+	public static int factorial(int n) {
+		int product = 1;
+		for(int i = 2; i <= n; i++) {
+			product *= i;
+		}
+
+		return product;
+	}
+
 	public static void main(String[] args) {
 		int n = 5;
 		
@@ -37,5 +46,7 @@ public class NestedLoops {
 			}
 			System.out.println();
 		}
+
+		System.out.println(factorial(7));
 	}
 }
