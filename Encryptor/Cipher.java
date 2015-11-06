2e0d9832dd607dc3521325f7ef65659337814aa4
DarkAce65 <tvdood99@gmail.com>
2015-11-06 10:38:50 -0500

    Add test method

--- Encryptor/Cipher.java
+++ Encryptor/Cipher.java
@@ -1,5 +1,5 @@
 public class Cipher {
-	private String plaintext;
+	private static String plaintext;
 
 	public Cipher(String text) {
 		plaintext = text;
@@ -13,7 +13,7 @@ public class Cipher {
 		plaintext = text;
 	}
 
-	public String caesarShift(char letter) {
+	public static String caesarShift(char letter) {
 		String ciphertext = plaintext;
 		for(int i = 0; i < plaintext.length(); i++) {
 			System.out.println((int) plaintext.charAt(i));
@@ -24,6 +24,7 @@ public class Cipher {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("T");
+		plaintext = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		caesarShift('e');
 	}
 }
