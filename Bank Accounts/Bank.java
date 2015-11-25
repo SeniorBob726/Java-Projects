02d57ff33559a5c0dd987cb30480e64dfa6a6af7
DarkAce65 <tvdood99@gmail.com>
2015-11-24 21:20:02 -0500

    Begin creating interface

--- Bank Accounts/Bank.java
+++ Bank Accounts/Bank.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bank {
 	private BankAccount[] accounts;
 	private int numberCustomers;
@@ -26,13 +28,45 @@ public class Bank {
 			int number;
 			do {
 				number = (int)(Math.random() * 1000000);
-			} while(elementExistsInArray(accountNumbers, number));
+			} while(!elementExistsInArray(accountNumbers, number));
 
 			accounts[numberCustomers] = new BankAccount(name, address, number, balance);
 			numberCustomers++;
 			return number;
 		}
 		return 0;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Create a bank with a maximum size of: ");
+		Bank bank = new Bank(scanner.nextInt());
+
+		System.out.print("\n");
+		System.out.println("Welcome to the Bank! Type any of the following commands to start.");
+		System.out.println("\tAdd - Add an account to the bank");
+		System.out.println("\tDeposit - Deposit money into an account");
+		System.out.println("\tWithdraw - Withdraw money from an account");
+		System.out.println("\tExit - Exit the bank interface");
+
+		while(true) {
+			System.out.print("> ");
+			String input = scanner.next().toLowerCase();
+
+			if(input.equals("exit")) {
+				System.out.println("Goodbye!");
+				break;
+			}
+
+			if(input.equals("add")) {
+				System.out.print("Please enter your name: ");
+				String name = scanner.next();
+				System.out.print("Please enter your address: ");
+				String address = scanner.next();
+				System.out.println("Your account number is: " + bank.addAccount(name, address, 0));
+			}
+		}
 	}
 
 	/*int addAccount(String name, String address, double balance) - instantiates next element of the accounts[] array to a new BankAccount with this information. Updates numberCustomers. Return account number to user.  Ensure you don't go over capacity.
