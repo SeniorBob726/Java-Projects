df999b608db3fd610bb295f18437c9626cbf4fed
DarkAce65 <tvdood99@gmail.com>
2015-12-01 10:05:37 -0500

    Create Card class

--- Cards/Card.java
+++ Cards/Card.java
@@ -1,3 +1,19 @@
 public class Card {
+	private final int faceValue;
+	private final String name;
+	private final String suit;
 
+	public Card(int v, String n, String s) {
+		faceValue = v;
+		name = n;
+		suit = s;
+	}
+
+	public int getFaceValue() {
+		return faceValue;
+	}
+
+	public String toString() {
+		return name + " of " + suit;
+	}
 }
