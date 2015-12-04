86af7d9a149ee571d958657fff66989246dc67a6
DarkAce65 <tvdood99@gmail.com>
2015-12-04 10:30:38 -0500

    Create Player class

---
+++ Cards/Player.java
@@ -0,0 +1,51 @@
+public class Player {
+	private String name;
+	private Card[] hand;
+	private int size;
+	private int max_size;
+
+	public Player(String n, int max) {
+		name = n;
+		hand = new Card[max];
+		size = 0;
+		max_size = max;
+	}
+
+	public void addCard(Card c) {
+		hand[size] = c;
+		size++;
+	}
+
+	public Card discard(int i) {
+		Card c = hand[i];
+		hand[i] = null;
+		size--;
+
+		return c;
+	}
+
+	public Card[] discard() {
+		hand = new Card[max_size];
+		size = 0;
+
+		return hand;
+	}
+
+	public String showHand() {
+		String output = "";
+		for(int i = 0; i < size; i++) {
+			output += hand[i] + ", ";
+		}
+
+		return output.substring(0, output.length() - 2);
+	}
+
+	public void fixCards() {
+		int j = 0;
+		for(int i = 0; i < hand.length; i++) {
+			if(hand[i] != null) {
+				hand[j++] = hand[i];
+			}
+		}
+	}
+}
