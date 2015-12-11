bf4eeb7f5dcb2c0087b0d72be5529996bf492d9b
DarkAce65 <tvdood99@gmail.com>
2015-12-11 10:35:17 -0500

    Create isFlush method

--- Cards/GameRules.java
+++ Cards/GameRules.java
@@ -1,7 +1,15 @@
 public class GameRules {
-	public int processHand(Cards[] cards) {
-		for(int i = 0; i < cards.length; i++) {
-
+	private boolean isFlush() {
+		String check = hand[0].getSuit();
+		for(int i = 1; i < hand.length; i++) {
+			if(!check.equals(hand[i].getSuit())) {
+				return false;
+			}
 		}
+		return true;
+	}
+
+	public int processHand(Cards[] cards) {
+
 	}
 }
