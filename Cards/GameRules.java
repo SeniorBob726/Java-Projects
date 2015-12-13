b2a19ee757251684f7d3b4e947572f16cbf9b4ac
DarkAce65 <tvdood99@gmail.com>
2015-12-13 10:38:12 -0500

    Create isStraight and pairs methods

--- Cards/GameRules.java
+++ Cards/GameRules.java
@@ -1,5 +1,5 @@
 public class GameRules {
-	private boolean isFlush() {
+	private boolean isFlush(Card[] hand) {
 		String check = hand[0].getSuit();
 		for(int i = 1; i < hand.length; i++) {
 			if(!check.equals(hand[i].getSuit())) {
@@ -7,6 +7,34 @@ public class GameRules {
 			}
 		}
 		return true;
+	}
+
+	private boolean isStraight(Card[] hand) {
+		Card.sortByFaceValue(hand);
+
+		int check = hand[0].getFaceValue();
+		for(int i = 1; i < hand.length; i++) {
+			if(check != hand[i].getFaceValue() + i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private int pairs(Card[] hand) {
+		Card.sortByFaceValue(hand);
+		int pairs = 0;
+
+		int check = hand[0].getFaceValue();
+		for(int i = 1; i < hand.length; i++) {
+			if(check == hand[i].getFaceValue())) {
+				pairs++;
+				if(i + 1 < hand.length) {
+					check = hand[i + 1].getFaceValue();
+				}
+			}
+		}
+		return pairs;
 	}
 
 	public int processHand(Cards[] cards) {
