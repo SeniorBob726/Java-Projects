d8c442c25abfe85aaf6b0bdc7bba976dff072a67
DarkAce65 <tvdood99@gmail.com>
2015-12-13 12:20:51 -0500

    Create sortBySuit method without comparators

--- Cards/Card.java
+++ Cards/Card.java
@@ -25,6 +25,18 @@ public class Card {
 		return name + " of " + suit;
 	}
 
+	public static void sortBySuit(Card[] array) { // Sort cards in alphabetical order by suit
+		for(int i = 1; i < array.length; i++) {
+			for(int j = i; j > 0; j--) {
+				if(array[j].getSuit().compareTo(array[j - 1].getSuit()) > 0) {
+					Card temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
+				}
+			}
+		}
+	}
+
 	public static void sortByFaceValue(Card[] array) { // Sort cards in ascending order by faceValue
 		for(int i = 1; i < array.length; i++) {
 			for(int j = i; j > 0; j--) {
