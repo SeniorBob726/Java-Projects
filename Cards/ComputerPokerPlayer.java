6026cb1a00ce9e13516dee7020dc87468cc26139
DarkAce65 <tvdood99@gmail.com>
2015-12-13 21:09:46 -0500

    Start computer player

--- Cards/ComputerPokerPlayer.java
+++ Cards/ComputerPokerPlayer.java
@@ -1,5 +1,16 @@
 public class ComputerPokerPlayer extends PokerPlayer {
+	private GameRules rules;
+
 	public ComputerPokerPlayer(String name, int max, double b) {
 		super(name, max, b);
+		rules = new GameRules();
+	}
+
+	public double bet(Card[] hand) {
+		rules.processHand(hand);
+	}
+
+	public int[] playHand() {
+		rules.processHand(hand);
 	}
 }
