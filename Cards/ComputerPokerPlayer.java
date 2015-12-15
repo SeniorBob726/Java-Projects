1943c3853610ecd683b48056e5facd3ba47e5ce2
DarkAce65 <tvdood99@gmail.com>
2015-12-14 23:35:26 -0500

    Finish ComputerPokerPlayer AI

--- Cards/ComputerPokerPlayer.java
+++ Cards/ComputerPokerPlayer.java
@@ -15,6 +15,33 @@ public class ComputerPokerPlayer extends PokerPlayer {
 	public int[] decideDiscard() {
 		PokerHand pokerHand = rules.processHand(hand);
 
+		int size = 0;
+		int v = pokerHand.getHandValue();
+		if(v <= 1) { // High Card - discard all
+			int[] discard = {0, 1, 2, 3, 4};
+			return discard;
+		}
+		if(v == 2) { // One Pair - discard excess
+			int two = pokerHand.getTwoOfAKind()[0];
+			int[] discard = new int[3];
+			for(int i = 0; i < hand.length; i++) {
+				if(hand[i].getFaceValue() != two) {
+					discard[size++] = i;
+				}
+			}
+			return discard;
+		}
+		if(v == 4) { // Three Of A Kind - discard excess
+			int three = pokerHand.getThreeOfAKind()[0];
+			int[] discard = new int[2];
+			for(int i = 0; i < hand.length; i++) {
+				if(hand[i].getFaceValue() != three) {
+					discard[size++] = i;
+				}
+			}
+			return discard;
+		}
+
 		return new int[0];
 	}
 }
