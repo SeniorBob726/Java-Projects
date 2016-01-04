4284866c341cc900bd3c2923369848942369b2b8
DarkAce65 <tvdood99@gmail.com>
2016-01-03 19:02:00 -0500

    Fill in Vendor.java

--- Snackbar/Vendor.java
+++ Snackbar/Vendor.java
@@ -3,48 +3,51 @@
 * of items. A vendor carries out sales transactions.
 */
 public class Vendor {
-	// Fields:
-
+	private int change = 0;
+	private int deposit = 0;
+	private int pricePerUnit = 0;
+	private int amountStocked = 0;
 	/**
 	* Constructs a Vendor
-	* @param price the price of a single item in cents (int)
+	* @param price price of a single item in cents (int)
 	* @param stock number of items to place in stock (int)
 	*/
-	... Vendor ... {
-		...
+	public Vendor(int price, int stock) {
+		pricePerUnit = price;
+		amountStocked = stock;
 	}
 
 	/**
 	* Sets the quantity of items in stock.
 	* @param qty number of items to place in stock (int)
 	*/
-	... setStock ... {
-		...
+	public void setStock(int qty) {
+		amountStocked = qty;
 	}
 
 	/**
 	* Returns the number of items currently in stock.
 	* @return number of items currently in stock (int)
 	*/
-	... getStock ... {
-		...
+	public int getStock() {
+		return amountStocked;
 	}
 
 	/**
 	* Adds a specified amount (in cents) to the
 	* deposited amount.
-	* @param number of cents to add to the deposit (int)
+	* @param amount number of cents to add to the deposit (int)
 	*/
-	... addMoney ... {
-		...
+	public void addMoney(int amount) {
+		deposit += amount;
 	}
 
 	/**
 	* Returns the currently deposited amount (in cents).
 	* @return number of cents in the current deposit (int)
 	*/
-	... getDeposit ... {
-		...
+	public int getDeposit() {
+		return deposit;
 	}
 
 	/**
@@ -56,8 +59,15 @@ public class Vendor {
 	* (moves it into change) and returns false.
 	* @return true for a successful sale, false otherwise (boolean)
 	*/
-	... makeSale ... {
-		...
+	public boolean makeSale() {
+		if(amountStocked > 0 && deposit >= pricePerUnit) {
+			change = deposit - pricePerUnit;
+			deposit = 0;
+			return true;
+		}
+		change = deposit;
+		deposit = 0;
+		return false;
 	}
 
 	/**
@@ -65,7 +75,7 @@ public class Vendor {
 	* the last sale or refund).
 	* @return number of cents in the current change (int)
 	*/
-	... getChange ... {
-		...
+	public int getChange() {
+		return change;
 	}
 }
