4601d7a2ad9123705a8099a5e840fbbcb8de319b
DarkAce65 <tvdood99@gmail.com>
2016-01-13 10:38:41 -0500

    Start foundOrInserted method

--- IndexMaker/DocumentIndex.java
+++ IndexMaker/DocumentIndex.java
@@ -1,3 +1,25 @@
 public class DocumentIndex extends ArrayList<IndexEntry> {
+	public DocumentIndex() {
+		super();
+	}
 
+	public DocumentIndex(int capacity) {
+		super(capacity);
+	}
+
+	private int foundOrInserted(String word) {
+		int index = 0;
+		for(int i = 0; i < this.size(); i++) {
+			String entryWord = this.get(i).getWord().toUpperCase();
+			if(word.toUpperCase().equals(entryWord)) {
+				return i;
+			}
+		}
+
+		return index;
+	}
+
+	public void addWord(String word, int num) {
+		this.get(foundOrInserted(word)).add(num);
+	}
 }
