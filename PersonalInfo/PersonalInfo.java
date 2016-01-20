c165242525405ce1ecc96c4a472df91d40082cd7
DarkAce65 <tvdood99@gmail.com>
2016-01-20 11:32:56 -0500

    Add setter methods

--- PersonalInfo/PersonalInfo.java
+++ PersonalInfo/PersonalInfo.java
@@ -5,16 +5,16 @@ public class PersonalInfo {
 	private String lastName;
 	private Address address;
 	private ArrayList<String> phoneNumbers;
-	private ArrayList<String> emailAddress;
-	private ArrayList<String> website;
+	private ArrayList<String> emailAddresses;
+	private ArrayList<String> websites;
 
 	public PersonalInfo(String fn, String ln, String street, String city, String state, String county) {
 		firstName = fn;
 		lastName = ln;
 		address = new Address(street, city, state, county);
 		phoneNumbers = new ArrayList<String>();
-		emailAddress = new ArrayList<String>();
-		website = new ArrayList<String>();
+		emailAddresses = new ArrayList<String>();
+		websites = new ArrayList<String>();
 	}
 
 	public String getFirstName() {
@@ -33,11 +33,23 @@ public class PersonalInfo {
 		return phoneNumbers;
 	}
 
-	public ArrayList<String> getEmailAddress() {
-		return emailAddress;
+	public void addPhoneNumber(String pn) {
+		phoneNumbers.add(pn);
 	}
 
-	public ArrayList<String> getWebsite() {
-		return website;
+	public ArrayList<String> getEmailAddresses() {
+		return emailAddresses;
+	}
+
+	public void addEmailAddress(String ea) {
+		emailAddresses.add(ea);
+	}
+
+	public ArrayList<String> getWebsites() {
+		return websites;
+	}
+
+	public void addWebsite(String w) {
+		websites.add(w);
 	}
 }
