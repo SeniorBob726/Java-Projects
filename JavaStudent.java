e1426f302a6c69c5aaf0eb87acd5338bf6c76f79
DarkAce65 <tvdood99@gmail.com>
2016-01-20 09:50:17 -0500

    Create JavaStudent class

---
+++ JavaStudent.java
@@ -0,0 +1,47 @@
+public class JavaStudent {
+	private String name;
+	private ArrayList<String> favoriteMovies;
+
+	public JavaStudent(String n, String mv1, String mv2, String mv3) {
+		name = n;
+		favoriteMovies.add(mv1);
+		favoriteMovies.add(mv2);
+		favoriteMovies.add(mv3);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void addMovie(String m) {
+		if(!favoriteMovies.contains(m)) {
+			favoriteMovies.add(m);
+		}
+	}
+
+	public ArrayList<String> getMovies() {
+		return favoriteMovies;
+	}
+
+	public void removeLongTitles() {
+		for(int i = favoriteMovies.size(); i >= 0; i--) {
+			if(favoriteMovies.get(i).length() > 6) {
+				favoriteMovies.remove(i);
+			}
+		}
+	}
+
+	public String toString()  {
+		String output = "Name: " + name + "\nMovies: ";
+
+		for(String mv : favoriteMovies) {
+			output += mv + ", ";
+		}
+
+		return output.substring(0, substring.length() - 2);
+	}
+}
