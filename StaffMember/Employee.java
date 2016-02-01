ea102f17b3c2e2d6e3ecf07f9d2d6969ac310145
DarkAce65 <tvdood99@gmail.com>
2016-02-01 10:26:36 -0500

    Complete Staff hierarchy

--- StaffMember/Employee.java
+++ StaffMember/Employee.java
@@ -1,3 +1,36 @@
 public class Employee extends StaffMember {
+	private String ssn;
+	private double wage;
 
+	public Employee(String name, String address, String phoneNumber, String s, double w) {
+		setName(name);
+		setAddress(address);
+		setPhoneNumber(phoneNumber);
+		ssn = s;
+		wage = w;
+	}
+
+	public void setSSN(String s) {
+		ssn = s;
+	}
+
+	public void setWage(double w) {
+		wage = w;
+	}
+
+	public String getSSN() {
+		return ssn;
+	}
+
+	public double getWage() {
+		return wage;
+	}
+
+	public double pay() {
+		return wage;
+	}
+
+	public String toString() {
+		return super.toString() + "\nSSN: " + ssn;
+	}
 }
--- StaffMember/Executive.java
+++ StaffMember/Executive.java
@@ -1,3 +1,9 @@
 public class Executive extends Employee {
+	public Executive(String name, String address, String phoneNumber, String ssn, double wage) {
+		super(name, address, phoneNumber, ssn, wage);
+	}
 
+	public void awardBonus(double bonus) {
+		setWage(getWage() + bonus);
+	}
 }
