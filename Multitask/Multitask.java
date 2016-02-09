1c605d2eee6cdefb7d91bb3cc2f239ef3ad7491e
DarkAce65 <tvdood99@gmail.com>
2016-02-09 11:34:17 -0500

    Implement GridLayout logic

--- Multitask/Multitask.java
+++ Multitask/Multitask.java
@@ -1,23 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Multitask {
-	private ArrayList<MiniGame> games;
-
-	public static void main(String[] args) {
-		JFrame arcade = new JFrame();
-		arcade.setTitle("Multitask");
-		arcade.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	public static void createPane(JFrame arcade, ArrayList<MiniGame> games) {
 		Container pane = arcade.getContentPane();
-		pane.setLayout(new GridLayout(games.length() / 2, games.length())); // Figure out math for GridLayout
+		int rows = 1, cols = games.size();
+		if(games.size() > 2) {
+			rows = 2;
+			cols /= 2;
+		}
+		pane.setLayout(new GridLayout(rows, cols));
+
+		games.get(0).setFirstGame(true);
 
 		for(MiniGame game : games) {
 			pane.add(game);
 		}
 
+		JTextField text = new JTextField();
+		pane.add(text);
+
 		arcade.pack();
 		arcade.setVisible(true);
-		panel.requestFocus();
+	}
+
+	public static void main(String[] args) {
+		ArrayList<MiniGame> games = new ArrayList<MiniGame>();
+		MiniGame game = new Balance();
+		games.add(game);
+
+		JFrame arcade = new JFrame("Multitask");
+		arcade.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		createPane(arcade, games);
 	}
 }
