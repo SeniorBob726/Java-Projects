4c38a495bf73c952b634d26437268e3aef6dae54
DarkAce65 <tvdood99@gmail.com>
2016-02-09 11:34:43 -0500

    Add firstGame property

--- Multitask/MiniGame.java
+++ Multitask/MiniGame.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public abstract class MiniGame extends JPanel implements KeyListener, ActionListener {
+public abstract class MiniGame extends JPanel implements KeyListener {
+	private boolean firstGame = false;
 
+	public boolean getFirstGame() {
+		return firstGame;
+	}
+
+	public void setFirstGame(boolean b) {
+		firstGame = b;
+	}
 }
