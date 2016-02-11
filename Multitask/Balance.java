5108b31e5f9629a98d9d6f6079272acdd036021a
DarkAce65 <tvdood99@gmail.com>
2016-02-11 11:36:21 -0500

    Start writing paintComponent method

--- Multitask/Balance.java
+++ Multitask/Balance.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Balance extends MiniGame {
+	public Balance() {
+		int width = 600;
+		int height = 450;
+
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		setBackground(Color.GRAY);
+		setPreferredSize(new Dimension(width, height));
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.setColor(new Color(50, 50, 50));
+		g.fillRect(0, 0, 50, 70);
+	}
 }
--- Multitask/MiniGame.java
+++ Multitask/MiniGame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class MiniGame extends JPanel {
 	public abstract void paintComponent(Graphics g);
