3db9d672a3534ad6e40192f4c61319ea220fa924
DarkAce65 <tvdood99@gmail.com>
2016-02-11 12:38:11 -0500

    Start using Graphics2D

--- Multitask/Balance.java
+++ Multitask/Balance.java
@@ -1,21 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Balance extends MiniGame {
 	public Balance() {
-		int width = 600;
-		int height = 450;
-
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
 		setBackground(Color.GRAY);
-		setPreferredSize(new Dimension(width, height));
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		g.setColor(new Color(50, 50, 50));
-		g.fillRect(0, 0, 50, 70);
+		int originX = getWidth() / 2;
+		int originY = getHeight() / 2;
+
+		int xOffset = -(getWidth() / 4);
+		int yOffset = -(10 / 2);
+
+		g.setColor(Color.BLACK);
+		Graphics2D g2d = (Graphics2D) g.create();
+
+		g2d.translate(originX, originY);
+		g2d.rotate(Math.toRadians(60), 0, 0);
+
+		g2d.fill(new Rectangle2D.Double(xOffset, yOffset, getWidth() / 2, 10));
+		g2d.dispose();
 	}
 }
--- Multitask/MiniGame.java
+++ Multitask/MiniGame.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 public abstract class MiniGame extends JPanel {
-	public abstract void paintComponent(Graphics g);
 }
