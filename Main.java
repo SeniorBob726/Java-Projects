0edc2ff6248ba4929972780bcd0f8d163f9fa416
DarkAce65 <tvdood99@gmail.com>
2016-02-11 21:38:39 -0500

    Are objects passed by reference or by value?

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 public class Main {
+	public static void swap(String a, String b) {
+		String c = a;
+		a = b;
+		b = c;
+	}
+
 	public static void main(String[] args) {
-		int n = 4;
-		int[] squares = new int[n + 1];
-		for(int i = 0; i < squares.length; i++) {
-			squares[i] = i*i;
-		}
-		for(int i = squares.length - 1; i > 0; i--) {
-			if(i == 1) {
-				System.out.print(squares[i]);
-			}
-			else {
-				System.out.print(squares[i] + ", ");
-			}
-		}
+		String x = "Hello";
+		String y = "Goodbye";
+		System.out.println(x + y);
+		swap(x, y);
+		System.out.println(x + y);
 	}
 }
