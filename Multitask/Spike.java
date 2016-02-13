3d3d191e09f96aa5b81b832a008bef3f68f804bb
DarkAce65 <tvdood99@gmail.com>
2016-02-13 13:11:49 -0500

    Add AffineTransform and setTranslation method

--- Multitask/Spike.java
+++ Multitask/Spike.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.geom.AffineTransform;
 
 public class Spike extends Path2D.Double {
+	private AffineTransform transform;
+
 	public Spike(double direction, double length) {
 		direction *= Math.PI / 180;
 		double base = 2 * length / 3;
@@ -13,9 +16,16 @@ public class Spike extends Path2D.Double {
 		lineTo(-r * c - length / 3 * s, -r * s + length / 3 * c);
 		lineTo(-r * c + length / 3 * s, -r * s - length / 3 * c);
 		closePath();
+
+		transform = new AffineTransform();
 	}
 
 	public Spike(double direction) {
 		this(direction, 30);
+	}
+
+	public void setTranslation(double x, double y) {
+		transform.setToTranslation(x, y);
+		transform(transform);
 	}
 }
