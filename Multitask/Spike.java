465e5d79783c004f2107d4f9fb58bed31b9f5bd4
DarkAce65 <tvdood99@gmail.com>
2016-02-13 13:51:44 -0500

    Rewrite Spike class

--- Multitask/Spike.java
+++ Multitask/Spike.java
@@ -1,31 +1,47 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.AffineTransform;
 
 public class Spike extends Path2D.Double {
 	private AffineTransform transform;
+	private Point2D position;
+	private double direction;
+	private double speed;
+	private double radius;
 
-	public Spike(double direction, double length) {
-		direction *= Math.PI / 180;
-		double base = 2 * length / 3;
-		double r = length / 2;
-		double c = Math.cos(direction);
-		double s = Math.sin(direction);
-		moveTo(r * c, r * s);
-		lineTo(-r * c - length / 3 * s, -r * s + length / 3 * c);
-		lineTo(-r * c + length / 3 * s, -r * s - length / 3 * c);
+	public Spike(double x, double y, double dir, double sp, double r) {
+		transform = new AffineTransform();
+		position = new Point2D.Double(x, y);
+		direction = dir * Math.PI / 180;
+		speed = sp;
+		radius = r;
+
+		draw();
+	}
+
+	public Spike(double x, double y, double dir, double sp) {
+		this(x, y, dir, sp, 12);
+	}
+
+	public void draw() {
+		double rc = radius * Math.cos(direction);
+		double rs = radius * Math.sin(direction);
+
+		moveTo(rc, rs);
+		lineTo(-rc - rs, rc - rs);
+		lineTo(-rc + rs, -rc - rs);
 		closePath();
 
-		transform = new AffineTransform();
+		transform.setToTranslation(position.getX(), position.getY());
+		transform(transform);
 	}
 
-	public Spike(double direction) {
-		this(direction, 30);
-	}
+	public void update() {
+		position.setLocation(speed * Math.cos(direction), speed * Math.sin(direction));
 
-	public void setTranslation(double x, double y) {
-		transform.setToTranslation(x, y);
+		transform.setToTranslation(position.getX(), position.getY());
 		transform(transform);
 	}
 }
