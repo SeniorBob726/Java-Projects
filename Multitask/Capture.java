ac5d7e8c4879f8a0cf87e84705125fecca75d507
DarkAce65 <tvdood99@gmail.com>
2016-02-28 23:37:31 -0500

    Adjust timing for Capture

--- Multitask/Capture.java
+++ Multitask/Capture.java
@@ -63,7 +63,7 @@ public class Capture extends MiniGame {
 		for(int i = 0; i < squares.length; i++) {
 			if(squares[i] == null && elapsedms >= nextSquare) {
 				squares[i] = createSquare();
-				nextSquare += (Math.random() * 4000 + 1000) / kc;
+				nextSquare = elapsedms + (long) ((Math.random() * 4000 + 1000) / kc);
 			}
 			else if(squares[i] != null && squares[i].getBox().intersects(box.getX(), box.getY(), box.getWidth(), box.getHeight())) {
 				squares[i].stopCountdown();
